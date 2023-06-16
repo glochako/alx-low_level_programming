0939a5bb1d9f162d9b306cb4870793a4be609d17
Glory Ochako <kawiraochako@gmail.com>
2023-06-16 16:12:19 -0700

    More numbers

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - Prints numbers from 0 to 9
+ *
+ * Return: Always 0 (Success)
+ *
+ */
+
+int main(void)
+
+{
+	int i;
+	for (i = 29; i < 39; i++)
+	{
+		putchar(i);
+	}
+	
+	putchar('\n');
+	return (0);
+}
