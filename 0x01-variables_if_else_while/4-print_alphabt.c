7aeda7aa95dd882c360898a0a56c2265beac0286
Glory Ochako <kawiraochako@gmail.com>
2023-06-16 16:03:37 -0700

    Numbers

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ * main - Prints the alphabet except q and e
+ *
+ * Return: Always 0 (Success)
+ *
+ */
+
+int main (void)
+
+{
+	int i;
+
+	for (i = 97; i < 123; i++)
+	{
+		if (i != 101 && i != 113)
+		{
+			putchar(i);
+		}
+	}
+	putchar('\n');
+	return (0);
+}
