1c934452652505cc6de4b2f95823b8e0ab459faf
Glory Ochako <kawiraochako@gmail.com>
2023-06-20 13:32:57 -0700

    Putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -10,6 +10,14 @@
 int main(void)
 
 {
-	printf("_putchar\n");
+	putchar('_');
+	putchar('p');
+	putchar('u');
+	putchar('t');
+	putchar('c');
+	putchar('h');
+	putchar('a');
+	putchar('r');
+	putchar('\n');
 	return (0);
 }
