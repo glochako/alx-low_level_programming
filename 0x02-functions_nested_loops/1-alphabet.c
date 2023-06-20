1b0765954518c521bb38e97711965ff424853d38
Glory Ochako <kawiraochako@gmail.com>
2023-06-20 14:29:37 -0700

    Alphabet

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -7,10 +7,10 @@
 
 void print_alphabet(void)
 {
-	char letter;
-	for (letter = 'a'; letter <= 'z'; letter++)
+	char c;
+	for (c = 'a'; c <= 'z'; c++)
 	
-		_putchar('letter');
+		_putchar('c');
 	
 	_putchar('\n');
 }
