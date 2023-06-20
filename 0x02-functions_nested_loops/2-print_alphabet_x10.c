f19b8242d659b6427e2192b0fcd2952413c4c27c
Glory Ochako <kawiraochako@gmail.com>
2023-06-20 16:13:54 -0700

    Alphabet x10

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - Prints alphabet x10 times
+ * Return: void
+ */
+void print_alphabet_x10(void)
+{
+	char c;
+	int i = 0;
+	
+	while (i <= 0)
+	{
+	for (c = 'a'; c <= 'z'; c++)
+	{
+	_putchar('c');
+	}
+	_putchar('\n');
+	i++;
+	}
+}
