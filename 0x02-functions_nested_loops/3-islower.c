c5bffcbd7cf23686de33fab26b60487fda07a59f
Glory Ochako <kawiraochako@gmail.com>
2023-06-21 03:00:52 -0700

    Alphabetx

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -3,7 +3,7 @@
 /**
  * _islower - checks for lowercase character
  * @c - The character to be checked
- * Return: 1 for lowercase character or 0 for anything else
+ * Return: 1 for lowercase character, otherwise return 0
  */
 
 int _islower(int c)
