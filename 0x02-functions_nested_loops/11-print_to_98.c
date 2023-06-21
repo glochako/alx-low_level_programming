aa390619357b0f9e65e4a58889d906da901ca258
Glory Ochako <kawiraochako@gmail.com>
2023-06-21 11:42:58 -0700

    Update

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -17,9 +17,10 @@ void print_to_98(int n)
 		for (i = n; i <= 98; i++)
 		{
 			if (i != 98)
-				printf("%d, ", i);
-                                else if (i == 98)
-                                        printf("%d\n", i);
+			printf("%d, ", i);
+			
+			else if (i == 98)
+			printf("%d\n", i);
 		}
 	} else if (n >= 98)
                 {
