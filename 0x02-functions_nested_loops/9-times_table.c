b64406a53479ae33815b757c415f8b6dc3448187
Glory Ochako <kawiraochako@gmail.com>
2023-06-21 07:50:32 -0700

    Update

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -27,12 +27,10 @@ void times_table(void)
 			{
 				_putchar(',');
 				_putchar(' ');
-				_putchar((k/10 + '0'));
-				_putchar((k%10) + '0');
-			}
-			{
-				_putchar('\n');
+				_putchar((k / 10) + '0');
+				_putchar((k % 10) + '0');
 			}
 		}
+		_putchar('\n');
 	}
-}	
+}
