9f777546d64c94a700b7515725940d32be668849
Glory Ochako <kawiraochako@gmail.com>
2023-06-22 14:53:10 -0700

    Update

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * mul - Multiplies two integers
+ * @a: - The integer to be multiplied
+ * @b: -  The integer to be multiplied
+ * Return: Result of multiplication
+ */
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
