4df1ffdcea1ba1ebbdf2ad35dd7cb7bbce52a119
Glory Ochako <kawiraochako@gmail.com>
2023-06-23 15:00:54 -0700

    Update

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -14,15 +14,15 @@ int main(void)
 
 	for (i = 1; 1 <= 100; i++)
 	{
-		if ((i % 3 == 0) && (i % 5 != 0))
-		{
-			printf("Fizz");
-		} else if ((i % 5 == 0) && (i % 3 != 0))
-		{
-			printf("Buzz");
-		} else if ((i % 3 == 0) && (i % 5 == 0))
+		if (i % 3 == 0 && i % 5 == 0)
 		{
 			printf("FizzBuzz");
+		} else if (i % 3 == 0)
+		{
+			printf("Fizz");
+		} else if (i % 5 == 0)
+		{
+			printf("Buzz");
 		} else
 		{
 			printf(" %d", i);
