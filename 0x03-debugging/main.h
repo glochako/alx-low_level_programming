5d934798405fe8b78e6366b01796a0ff0fd444ca
Glory Ochako <kawiraochako@gmail.com>
2023-06-23 15:57:32 -0700

    2

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -4,5 +4,8 @@
 #include <stdio.h>
 
 void positive_or_negative(int i);
+int largest_number(int a, int b, int c);
+void print_remaining_days(int mont, int day, int year);
+int convert_day(int month, int day);
 
 #endif
