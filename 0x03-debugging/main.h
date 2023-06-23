4d013c56d0dedb36e19423f6135ff4ee95a396d2
Glory Ochako <kawiraochako@gmail.com>
2023-06-23 16:26:09 -0700

    One

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -1,6 +1,5 @@
 #ifndef MAIN_H
 #define MAIN_H
-
 #include <stdio.h>
 
 void positive_or_negative(int i);
@@ -8,4 +7,4 @@ int largest_number(int a, int b, int c);
 void print_remaining_days(int mont, int day, int year);
 int convert_day(int month, int day);
 
-#endif
+#endif /* MAIN_H */
