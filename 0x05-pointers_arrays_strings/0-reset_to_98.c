24f87fdb5697f2733c37eef346e8b514c155e061
Glory Ochako <kawiraochako@gmail.com>
2023-06-26 14:31:11 -0700

    BAck to 98

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - A function that takes a pointer to an int as parameter
+ * updates the value it points to to 98
+ * @n: - input
+ * Return: n
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
---
+++ 0x05-pointers_arrays_strings/README.md
@@ -0,0 +1,1 @@
+C- Pounters, arrays and strings
