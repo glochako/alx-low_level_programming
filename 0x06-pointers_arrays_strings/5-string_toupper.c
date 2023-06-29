92a7e54e12099bd72303ec159cabf4d2b9ef4d79
Glory Ochako <kawiraochako@gmail.com>
2023-06-29 09:34:28 -0700

    6 & 7

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -10,10 +10,10 @@ char *string_toupper(char *n)
 {
 	int i = 0;
 
-	while (n(i) != 0)
+	while (n[i] != '\0')
 	{
 		if (n[i] >= 'a' && n[i] <= 'z')
-		n[i] == 32;
+		n[i] = n[i] - 32;
 		i++;
 	}
 	return (0);
