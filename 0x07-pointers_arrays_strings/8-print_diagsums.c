a8afb2bcd2087af9cb837169eecc7524eb00a56a
Glory Ochako <kawiraochako@gmail.com>
2023-07-05 05:54:28 -0700

    Diagsums

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -20,7 +20,8 @@ void print_diagsums(int *a, int size)
 		sum1 = sum1 + a[y * size + y];
 	}
 	for (y = size - 1; y >= 0; y--)
-	{ sum2 += a[y * size + (size - y - 1)];
+	{
+		sum2 += a[y * size + (size - y - 1)];
 	}
-	printf("%d. %d\n", sum1, sum2);
+	printf("%d, %d\n", sum1, sum2);
 }
