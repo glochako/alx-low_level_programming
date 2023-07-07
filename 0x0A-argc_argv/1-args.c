77e8e7c38d30890f9967ca03b1d28b1876e56f7b
Glory Ochako <kawiraochako@gmail.com>
2023-07-07 15:33:42 -0700

    Args

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - prints the number of arguments passed to the program
+ * @argc: array of arguments
+ * @argv: number of arguments
+ * Return: Always 0 (Success)
+ */
+
+int main(int argc, char *argv[])
+{
+	int i;
+
+	for (i = 0; i < argc; i++)
+	{
+		printf("%s\n", argv[i]);
+	}
+	return (0);
+}
---
+++ 0x0A-argc_argv/main.h
@@ -0,0 +1,7 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+int _putchar(int c);
+int atoi(char *s);
+
+#endif
