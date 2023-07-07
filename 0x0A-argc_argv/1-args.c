18983c9c5a5b2fcee2caa7584764c4d885566b28
Glory Ochako <kawiraochako@gmail.com>
2023-07-07 15:41:03 -0700

    Args

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -10,11 +10,8 @@
 
 int main(int argc, char *argv[])
 {
-	int i;
+(void) argv; /*Ignore argv*/
+	printf("%d\n", argc - 1);
 
-	for (i = 0; i < argc; i++)
-	{
-		printf("%s\n", argv[i]);
-	}
 	return (0);
 }
