2e32d8d01d1f0df018a10d800c8001823216db6f
Glory Ochako <kawiraochako@gmail.com>
2023-07-07 13:07:00 -0700

    Static library

---
+++ 0x09-static_libraries/3-islower.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _islower - Checks for lowercase character
+ * @c: - The character to be checked
+ * Return: 1 if character is lowercase, otherwise return 0
+ */
+
+int _islower(int c)
+{
+	if (c >= 'a' && c <= 'z')
+		return (1);
+	else
+		return (0);
+}
