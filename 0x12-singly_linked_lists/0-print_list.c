708474231b090b62109dc4ca493a57604d59a866
Glory Ochako <kawiraochako@gmail.com>
2023-07-27 13:54:39 -0700

    Linked Lists

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -11,12 +11,9 @@ size_t print_list(const list_t *h)
 {
 	size_t s = 0;
 
-	while (h)
+	while (h != NULL)
 	{
-		if (!h->str)
-			printf("[0] (nil)\n");
-		else
-			printf("[%u] %s\n", h->len, h->str);
+		printf("[%d] %s\n", h->len, h->str != NULL ? h->str : "(nil)");
 		h = h->next;
 		s++;
 	}
