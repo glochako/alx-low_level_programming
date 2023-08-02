0d17fe7864f29832adc8c37a226ec756c936e8fa
Glory Ochako <kawiraochako@gmail.com>
2023-08-02 10:46:49 -0700

    Recursion

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -3,7 +3,7 @@
 /**
  * _puts_recursion - prints a string, followed by new line
  * @s: the string to be printed
- * Return: no return
+ * Return: string
  */
 
 void _puts_recursion(char *s)
@@ -14,5 +14,5 @@ void _puts_recursion(char *s)
 		_puts_recursion(s + 1);
 	}
 	else
-		_putchar('\n');
+	_putchar('\n');
 }
