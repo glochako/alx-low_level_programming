e534577e8a590e313c721619064d383846b46857
Glory Ochako <kawiraochako@gmail.com>
2023-08-21 03:13:56 -0700

    Variadic Functions

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -20,7 +20,7 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 		if (!separator)
 			printf("%d", va_arg(args, int));
 		else if (separator && i == 0)
-			printf("%d", va_arg(args,int));
+			printf("%d", va_arg(args, int));
 		else
 			printf("%s%d", separator, va_arg(args, int));
 	}
