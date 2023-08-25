6d1400b848f7e59b44998f77f04287aea64d86b2
Glory Ochako <kawiraochako@gmail.com>
2023-08-25 05:01:43 -0700

    Linked Lists

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -16,15 +16,15 @@ list_t *add_node(list_t **head, const char *str)
 	while (str[len])
 		len++;
 	new = malloc(sizeof(list_t));
-	if(new == NULL)
+	if (new == NULL)
 	{
 		return (NULL);
 	}
-	
+
 	new->str = strdup(str);
 	new->len = len;
 	new->next = (*head);
 	(*head) = new;
-	
+
 	return (*head);
 }
--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -15,19 +15,19 @@ list_t *add_node_end(list_t **head, const char *str)
 	list_t *new;
 	list_t *temporary = *head;
 	unsigned int len = 0;
-	
+
 	while (str[len])
 		len++;
 
 	new = malloc(sizeof(list_t));
-	
+
 	if (new == NULL)
 		return (NULL);
-	
+
 	new->str = strdup(str);
 	new->len = len;
 	new->next = NULL;
-	
+
 	if (*head == NULL)
 	{
 		*head = new;
@@ -37,6 +37,6 @@ list_t *add_node_end(list_t **head, const char *str)
 		temporary = temporary->next;
 
 	temporary->next = new;
-	
+
 	return (new);
 }
