d3b9ba72c278dd2996e5d66049225d8cd9ba93ff
Glory Ochako <kawiraochako@gmail.com>
2023-08-25 05:42:56 -0700

    Linked Lists

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -11,12 +11,12 @@ size_t print_list(const list_t *h)
 {
 	size_t node_count = 0;/* the node count starts at 0*/
 
-	while (h != NULL) /* iterates through the loop as long as h is not NULL*/
+	while (h) /* iterates through the loop as long as h is not NULL*/
 	{
-		if (h->str == NULL)/* would mean that the string is empty*/
-			printf("[0](nil)\n");
+		if (!h->str)/* would mean that the string is empty*/
+			printf("[0] (nil)\n");
 		else
-			printf("[%d] %s\n", h->len, h->str);/*prints the length of the string*/
+			printf("[%u] %s\n", h->len, h->str);/*prints the length of the string*/
 		h = h->next;
 		node_count++;
 	}
