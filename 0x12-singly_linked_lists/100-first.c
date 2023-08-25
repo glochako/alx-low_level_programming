b0f16048b8c779c1cdf3818d8800250295b172f1
Glory Ochako <kawiraochako@gmail.com>
2023-08-25 05:28:44 -0700

    Linked Lists

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+/**
+ * print - prints a sentence before the main is executed
+ */
+
+void print(void)
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("You're beat! and yet, you must allow,\n");
+}
