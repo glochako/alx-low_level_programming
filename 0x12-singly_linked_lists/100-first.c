c62c568a4a679f07236bd765cfda80ccb77a86e3
Glory Ochako <kawiraochako@gmail.com>
2023-08-25 05:35:59 -0700

    Linked Lists

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,11 +1,13 @@
 #include <stdio.h>
 
+void first(void) __attribute__((constructor));
+
 /**
- * print - prints a sentence before the main is executed
+ * first - prints a sentence before the main is executed
  */
 
-void print(void)
+void first(void)
 {
 	printf("You're beat! and yet, you must allow,\n");
-	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
 }
