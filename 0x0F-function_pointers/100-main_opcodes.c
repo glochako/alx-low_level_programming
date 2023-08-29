8b5608a3350357accc3d52eef77b70f2f5e53a03
Glory Ochako <kawiraochako@gmail.com>
2023-08-29 06:18:10 -0700

    Function Pointers

---
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -0,0 +1,41 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * main - prints its own opcodes
+ * @argc: number of arguments
+ * @argv: array of arguments
+ * Return: Always 0
+ */
+
+int main(int argc, char *argv[])
+{
+	int no_bytes, i;
+	char *a;
+
+	if (argc != 2)
+	{
+		printf("Error\n");
+		exit(1);
+	}
+	no_bytes = atoi(argv[1]);
+
+	if (no_bytes < 0)
+	{
+		printf("Error\n");
+		exit(2);
+	}
+
+	a = (char *)main;
+
+	for (i = 0; i < no_bytes; i++)
+	{
+		if (i == no_bytes - 1)
+		{
+			printf("%02hhx\n", a[i]);
+			break;
+		}
+		printf("%02hhx", a[i]);
+	}
+	return (0);
+}
