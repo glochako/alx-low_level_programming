ebf9ad56a3251c6e82f475188dbe29b4b1f3907d
Glory Ochako <kawiraochako@gmail.com>
2023-09-04 10:45:23 -0700

    File I/O

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * create_file - function that creates a file
+ * @filename: the file
+ * @text_content: NULL terminated string to write to the file
+ * Return: 1 (Success), or -1 (Failure)
+ */
+
+int create_file(const char *filename, char *text_content)
+{
+	int fp, wr, len = 0;
+
+	if (filename == NULL || text_content == NULL)
+		return (-1);
+	else
+	{
+		for (len = 0; text_content[len];)
+			len++;
+	}
+	fp = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
+	wr = write(fp, text_content, len);
+
+	if (fp == -1 || wr == -1)
+		return (-1);
+
+	close (fp);
+	return (1);
+}
