92df70e11ad0cb735841b1d3b529d645ce1b2ab9
Glory Ochako <kawiraochako@gmail.com>
2023-09-04 13:04:58 -0700

    File I/O

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -11,12 +11,15 @@ int append_text_to_file(const char *filename, char *text_content)
 {
 	int fp, wr, len = 0;
 
-	if (filename == NULL || text_content == NULL)
+	if (filename == NULL)
 		return (-1);
+
+	if (text_content != NULL)
 	{
 		for (len = 0; text_content[len] != '\0';)
 			len++;
 	}
+
 	fp = open(filename, O_WRONLY | O_APPEND);
 	wr = write(fp, text_content, len);
 
