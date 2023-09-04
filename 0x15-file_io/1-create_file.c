244fae0413388796c40d364813f2c1dd11190db6
Glory Ochako <kawiraochako@gmail.com>
2023-09-04 10:51:01 -0700

    File I/O

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -11,9 +11,9 @@ int create_file(const char *filename, char *text_content)
 {
 	int fp, wr, len = 0;
 
-	if (filename == NULL || text_content == NULL)
+	if (filename == NULL)
 		return (-1);
-	else
+	if (text_content != NULL)
 	{
 		for (len = 0; text_content[len];)
 			len++;
