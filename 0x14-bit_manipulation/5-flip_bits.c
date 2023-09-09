ffdd5c1a70ce40bac05f4c6da8178bccf8347bd1
Glory Ochako <kawiraochako@gmail.com>
2023-09-09 05:58:33 -0700

    Bit Manipulation

---
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * flip_bits - returns the number of  bits to be flipped
+ * to get from one number to another
+ * @n: first number
+ * @m: second number
+ * Return: number of bits
+ */
+
+unsigned int flip_bits(unsigned long int n, unsigned long int m)
+{
+	int i, counter = 0;
+	unsig
